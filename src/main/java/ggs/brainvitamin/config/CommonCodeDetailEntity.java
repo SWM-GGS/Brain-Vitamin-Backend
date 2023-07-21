f79817dec8888560c9d07bea1736afa989f275ce
gyujegal <lkjdee0329@gmail.com>
2023-07-21 18:28:49 +0900

    Feat: CommonCode 관련 엔티티 생성

---
+++ src/main/java/ggs/brainvitamin/config/CommonCodeDetailEntity.java
@@ -0,0 +1,24 @@
+package ggs.brainvitamin.config;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "commonCodeDetail")
+@Getter
+public class CommonCodeDetailEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @Column(nullable = false)
+    private String codeDetail;
+
+    @Column(nullable = false)
+    private String codeDetailName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private CommonCodeEntity commonCode;
+}
---
+++ src/main/java/ggs/brainvitamin/config/CommonCodeEntity.java
@@ -0,0 +1,25 @@
+package ggs.brainvitamin.config;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "commonCode")
+@Getter
+public class CommonCodeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition = "INT UNSIGNED")
+    private Integer id;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String codeName;
+
+    @OneToMany(mappedBy = "commonCode", cascade = CascadeType.ALL)
+    private List<CommonCodeDetailEntity> commonCodeDetailEntities;
+}
