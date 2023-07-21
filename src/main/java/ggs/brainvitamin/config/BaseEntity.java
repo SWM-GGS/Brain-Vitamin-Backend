f1920b814eac96185744fed77452882a43a4e234
gyujegal <lkjdee0329@gmail.com>
2023-07-21 18:29:17 +0900

    Feat: 각 엔티티에 상속받을 BaseEntity 생성

---
+++ src/main/java/ggs/brainvitamin/config/BaseEntity.java
@@ -0,0 +1,24 @@
+package ggs.brainvitamin.config;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "createdAt", updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private CommonCodeDetailEntity commonCodeDetail;
+}
