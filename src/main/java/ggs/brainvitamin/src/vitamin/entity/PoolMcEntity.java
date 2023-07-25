1626257678fe49c5c3d4d3728e1f431ad4a30f50
gyujegal <lkjdee0329@gmail.com>
2023-07-25 20:18:15 +0900

    Feat: PoolMcEntity 추가 객관식(Multiple Choice) 문제 Pool 저장

---
+++ src/main/java/ggs/brainvitamin/src/vitamin/entity/PoolMcEntity.java
@@ -0,0 +1,31 @@
+package ggs.brainvitamin.src.vitamin.entity;
+
+import ggs.brainvitamin.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "pool_MC")
+@NoArgsConstructor
+public class PoolMcEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id")
+    private ProblemEntity problem;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false, columnDefinition = "CHAR(1)")
+    private String answer;
+
+    @Column(nullable = false)
+    private String imgUrl;
+}
