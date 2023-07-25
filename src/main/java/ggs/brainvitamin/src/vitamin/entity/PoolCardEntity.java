8bf9719895b68a9843ad942322d0272d50825d3c
gyujegal <lkjdee0329@gmail.com>
2023-07-25 20:19:19 +0900

    Feat: PoolCardEntity 추가 카드 뒤집기 문제 Pool 저장

---
+++ src/main/java/ggs/brainvitamin/src/vitamin/entity/PoolCardEntity.java
@@ -0,0 +1,26 @@
+package ggs.brainvitamin.src.vitamin.entity;
+
+import ggs.brainvitamin.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "pool_CARD")
+@NoArgsConstructor
+public class PoolCardEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id")
+    private ProblemEntity problem;
+
+    @Column(nullable = false)
+    private String imgUrl;
+
+}
