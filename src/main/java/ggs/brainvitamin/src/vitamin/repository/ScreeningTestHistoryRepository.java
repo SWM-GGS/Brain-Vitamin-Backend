c20098d665230d51a2d7fa748f3790bae510319b
gyujegal <lkjdee0329@gmail.com>
2023-07-26 17:51:29 +0900

    Refactor: ScreeningTestHistoryRepository 수정 ScreeningTestHistoryRepository-> ScreeningTestHistoryEntity

--- src/main/java/ggs/brainvitamin/src/vitamin/repository/ScreeningTestHistoryRepository.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/repository/ScreeningTestHistoryRepository.java
@@ -1,9 +1,10 @@
 package ggs.brainvitamin.src.vitamin.repository;
 
+import ggs.brainvitamin.src.vitamin.entity.ScreeningTestHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ScreeningTestHistoryRepository extends JpaRepository<ScreeningTestHistoryRepository, Long> {
+public interface ScreeningTestHistoryRepository extends JpaRepository<ScreeningTestHistoryEntity, Long> {
 
 }
