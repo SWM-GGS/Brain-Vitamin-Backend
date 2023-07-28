66580c8ec838dadd3f6c949b9997b44c8637af3b
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-07-28 13:14:49 +0900

    Feat: 환자용 사용자 기능 dto 추가

---
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/ActivitiesDto.java
@@ -0,0 +1,9 @@
+package ggs.brainvitamin.src.user.patient.dto;
+
+import java.util.List;
+
+public class ActivitiesDto {
+
+    List<Boolean> weeklyPaticipants;
+
+}
---
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/ProfileImgDto.java
@@ -0,0 +1,4 @@
+package ggs.brainvitamin.src.user.patient.dto;
+
+public class ProfileImgDto {
+}
