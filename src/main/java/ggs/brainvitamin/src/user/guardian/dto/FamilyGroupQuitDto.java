20cefc486021425a0d2b31f2453367fac86f8b75
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-01 16:45:33 +0900

    Feat: 보호자앱 가족 그룹 탈퇴 관련 Dto

---
+++ src/main/java/ggs/brainvitamin/src/user/guardian/dto/FamilyGroupQuitDto.java
@@ -0,0 +1,11 @@
+package ggs.brainvitamin.src.user.guardian.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class FamilyGroupQuitDto {
+
+    private Long familyId;
+}
