0673cc1c787bca6988e77a6dc510044a8d0bd1f0
gyujegal <lkjdee0329@gmail.com>
2023-08-01 20:53:55 +0900

    Feat: 두뇌 비타민 인지 향상 게임 데이터 조회 API 구현 uri : /patient/vitamins/cog-training method : GET response : List<GetCogTrainingDto>

--- src/main/java/ggs/brainvitamin/src/vitamin/repository/VitaminAnalyticsRepository.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/repository/VitaminAnalyticsRepository.java
@@ -1,10 +1,15 @@
 package ggs.brainvitamin.src.vitamin.repository;
 
+import ggs.brainvitamin.src.user.entity.UserEntity;
 import ggs.brainvitamin.src.vitamin.entity.VitaminAnalyticsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VitaminAnalyticsRepository extends JpaRepository<VitaminAnalyticsEntity, Long> {
+
+    List<VitaminAnalyticsEntity> findTop5ByUserOrderByCreatedAtDesc(UserEntity userEntity);
 
 }
