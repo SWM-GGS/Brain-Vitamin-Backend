57b6533be600c0d105998164eba26ab0c146fc55
gyujegal <lkjdee0329@gmail.com>
2023-08-01 20:49:14 +0900

    Feat: PoolMcRepository 메서드 추가 pool_mc 테이블에서 랜덤으로 8개의 데이터 가져오는 findRandom8ByProblem 메서드 추가

--- src/main/java/ggs/brainvitamin/src/vitamin/repository/PoolMcRepository.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/repository/PoolMcRepository.java
@@ -2,8 +2,15 @@ package ggs.brainvitamin.src.vitamin.repository;
 
 import ggs.brainvitamin.src.vitamin.entity.PoolMcEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PoolMcRepository extends JpaRepository<PoolMcEntity, Long> {
+
+    @Query(value = "SELECT * FROM pool_mc where problem_id = :problemId order by RAND() limit 8", nativeQuery = true)
+    List<PoolMcEntity> findRandom8ByProblem(@Param(value = "problemId") Long problemId);
 }
