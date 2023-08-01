6cfb53239014c5925a99a6348eba1ae38fea60ac
gyujegal <lkjdee0329@gmail.com>
2023-08-01 20:48:05 +0900

    Feat: PoolCardRepository에 메서드 추가 pool_card 테이블에서 랜덤으로 6개의 데이터 가져오는 findRandom6ByProblem 메서드 추가

--- src/main/java/ggs/brainvitamin/src/vitamin/repository/PoolCardRepository.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/repository/PoolCardRepository.java
@@ -2,8 +2,14 @@ package ggs.brainvitamin.src.vitamin.repository;
 
 import ggs.brainvitamin.src.vitamin.entity.PoolCardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PoolCardRepository extends JpaRepository<PoolCardEntity, Long> {
+    @Query(value = "SELECT * FROM pool_card pc where pc.problem_id = :problemId order by RAND() limit 6", nativeQuery = true)
+    List<PoolCardEntity> findRandom6ByProblem(@Param(value = "problemId") Long problemId);
 }
