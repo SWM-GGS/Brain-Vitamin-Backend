d6012352665efb1cf57b106866a0100f04f1d65c
gyujegal <lkjdee0329@gmail.com>
2023-08-01 21:00:17 +0900

    Refactor: /maze, /market 매핑 삭제 test 용도로 뒀던 매핑 삭제

--- src/main/java/ggs/brainvitamin/src/vitamin/controller/VitaminController.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/controller/VitaminController.java
@@ -21,33 +21,6 @@ public class VitaminController {
 
     private final VitaminService vitaminService;
 
-    @GetMapping("/maze")
-    public BaseResponse<MazeDto> getMaze() {
-        try {
-            MazeDto mazeDto = new MazeDto("https://us.123rf.com/450wm/bonumopus/bonumopus1703/bonumopus170300778/75021750-%EB%AF%B8%EB%A1%9C-%EA%B2%8C%EC%9E%84-%EB%B0%B0%EA%B2%BD%EC%9E%85%EB%8B%88%EB%8B%A4-%EC%9E%85%EA%B5%AC%EC%99%80-%EC%B6%9C%EA%B5%AC%EC%99%80-%EB%AF%B8%EB%A1%9C%EC%9E%85%EB%8B%88%EB%8B%A4.jpg");
-
-            return new BaseResponse<>(mazeDto);
-        } catch (BaseException e) {
-            return new BaseResponse<>(e.getStatus());
-        }
-    }
-
-    @GetMapping("/market")
-    public BaseResponse<List<MarketDto>> getMarket() {
-        try {
-            MarketDto marketDto1 = new MarketDto("채소", "양파", 1000, 2, "https://img.lovepik.com/free-png/20210918/lovepik-onion-png-image_400259430_wh1200.png");
-            MarketDto marketDto2 = new MarketDto("채소", "당근", 2000, 3, "https://w7.pngwing.com/pngs/983/723/png-transparent-carrot-illustration-carrot-food-painted-hand.png");
-
-            ArrayList<MarketDto> marketDtos = new ArrayList<>();
-            marketDtos.add(marketDto1);
-            marketDtos.add(marketDto2);
-
-            return new BaseResponse<>(marketDtos);
-        } catch (BaseException e) {
-            return new BaseResponse<>(e.getStatus());
-        }
-    }
-
     /**
      * 환자 홈 화면 조회
      * @return GetPatientHomeDto
