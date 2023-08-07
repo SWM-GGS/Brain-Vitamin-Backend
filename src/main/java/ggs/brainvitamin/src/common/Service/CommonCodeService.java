088a960f83ccdb5625e9ff22d33372ddcf399791
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-07 17:25:36 +0900

    Feat: 공통코드 상세 조회 관련 기능 추가

---
+++ src/main/java/ggs/brainvitamin/src/common/Service/CommonCodeService.java
@@ -0,0 +1,31 @@
+package ggs.brainvitamin.src.common.Service;
+
+import ggs.brainvitamin.src.common.dto.CommonCodeDetailDto;
+import ggs.brainvitamin.src.common.entity.CommonCodeDetailEntity;
+import ggs.brainvitamin.src.common.repository.CommonCodeDetailRepository;
+import ggs.brainvitamin.src.common.repository.CommonCodeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommonCodeService {
+
+    private final CommonCodeRepository commonCodeRepository;
+    private final CommonCodeDetailRepository commonCodeDetailRepository;
+
+    public CommonCodeDetailDto getCodeWithCodeDetailName(String codeDetailName) {
+
+        CommonCodeDetailEntity codeDetailEntity =
+                commonCodeDetailRepository.findCommonCodeDetailEntityByCodeDetailName(codeDetailName);
+
+        CommonCodeDetailDto codeDetailDto = CommonCodeDetailDto.builder()
+                .id(codeDetailEntity.getId())
+                .codeDetail(codeDetailEntity.getCodeDetail())
+                .codeDetailName(codeDetailEntity.getCodeDetailName())
+                .commonCode(codeDetailEntity.getCommonCode())
+                .build();
+
+        return codeDetailDto;
+    }
+}
