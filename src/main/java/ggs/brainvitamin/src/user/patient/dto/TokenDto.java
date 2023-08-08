527358478415b827965792fca8227354624abfe2
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-08 17:46:28 +0900

    Feat: logout 기능 관련 Dto 수정

--- src/main/java/ggs/brainvitamin/src/user/patient/dto/TokenDto.java
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/TokenDto.java
@@ -2,6 +2,8 @@ package ggs.brainvitamin.src.user.patient.dto;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @Builder
@@ -9,6 +11,22 @@ import lombok.*;
 @NoArgsConstructor
 public class TokenDto {
 
-    private String accessToken;
-    private String refreshToken;
+    private AccessTokenDto accessTokenDto;
+    private RefreshTokenDto refreshTokenDto;
+
+    @Data
+    @Builder
+    public static class AccessTokenDto {
+
+        private String accessToken;
+        private Long accessTokenExpiresTime;
+    }
+
+    @Data
+    @Builder
+    public static class RefreshTokenDto {
+
+        private String refreshToken;
+        private Long refreshTokenExpiresTime;
+    }
 }
