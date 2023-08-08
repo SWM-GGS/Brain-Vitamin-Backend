476d531e488e605dfddb7c4f69f8b9f7e1fb329d
gyujegal <lkjdee0329@gmail.com>
2023-08-08 15:46:06 +0900

    [#21]Feat: 잘못된 사용자 타입에 대한 에러 코드 추가

--- src/main/java/ggs/brainvitamin/config/BaseResponseStatus.java
+++ src/main/java/ggs/brainvitamin/config/BaseResponseStatus.java
@@ -97,6 +97,7 @@ public enum BaseResponseStatus {
     FAILED_TO_LOGIN(false,3010,"없는 아이디이거나 비밀번호가 틀렸습니다."),
 
     NOT_ACTIVATED_USER(false,3020,"유효한 사용자가 아닙니다."),
+    INVALID_USERTYPE(false,3025,"잘못된 사용자 타입입니다."),
     NOT_ACTIVATED_PROBLEM(false,3030,"유효한 문제가 아닙니다."),
 
 
