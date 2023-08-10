97c151ea7b68dc7c958dfd4de9031f33d72e23ce
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-10 21:59:10 +0900

    [#38]Feat: 환자 프로필 정보 수정 관련 요청 Dto 추가

---
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/ProfilesRequestDto.java
@@ -0,0 +1,11 @@
+package ggs.brainvitamin.src.user.patient.dto;
+
+import lombok.Data;
+
+@Data
+public class ProfilesRequestDto {
+
+    private String profileImgUrl;
+    private String nickname;
+    private String education;
+}
