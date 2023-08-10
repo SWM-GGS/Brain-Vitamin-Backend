00e9cc5fa8de3269d7da0cbf402671a302482055
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-10 21:59:25 +0900

    [#38]Feat: 환자 프로필 정보 수정 관련 요청 entity 함수 추가

--- src/main/java/ggs/brainvitamin/src/user/entity/UserEntity.java
+++ src/main/java/ggs/brainvitamin/src/user/entity/UserEntity.java
@@ -89,4 +89,10 @@ public class UserEntity extends BaseEntity {
         this.todayVitaminCheck = todayVitaminCheck;
     }
 
+    public void updateProfiles(String nickname, String profileImgUrl, CommonCodeDetailEntity educationCode) {
+        this.nickname = nickname;
+        this.profileImgUrl = profileImgUrl;
+        this.educationCode = educationCode;
+    }
+
 }
