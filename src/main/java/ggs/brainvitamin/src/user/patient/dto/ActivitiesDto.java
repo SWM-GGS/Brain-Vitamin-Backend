b36d4eb5ce7dbfc9f14c84a24abfcf6db3ac94a4
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-11 22:32:46 +0900

    Feat: 환자 내 활동 보기 기능 관련 dto 추가

--- src/main/java/ggs/brainvitamin/src/user/patient/dto/ActivitiesDto.java
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/ActivitiesDto.java
@@ -1,9 +1,53 @@
 package ggs.brainvitamin.src.user.patient.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ActivitiesDto {
 
-    List<Boolean> weeklyPaticipants;
+    @Data
+    @Builder
+    public static class ActivitiesResponseDto {
 
+        private GetWeeklyVitaminDto weeklyVitaminDto;
+        private GetChangesFromLastWeekDto changesFromLastWeekDto;
+        private GetScreeningTestHistoryDto screeningTestHistoryDto;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class GetChangesFromLastWeekDto {
+
+        private HashMap<String, Double> changedFromLastWeek = new HashMap<>();
+    }
+
+    @Data
+    @Builder
+    public static class GetWeeklyVitaminDto {
+
+        private List<Boolean> weeklyVitaminAttendance = new ArrayList<>();
+    }
+
+    @Data
+    @Builder
+    public static class GetScreeningTestHistoryDto {
+
+        private Integer totalScore;
+        private String state;
+        private String description = "";
+
+//        private Integer attentionScore;     // 집중력
+//        private Integer executiveScore;     // 집행능력
+//        private Integer languageScore;      // 언어능력
+//        private Integer memoryScore;        // 기억력
+//        private Integer orientationScore;   // 지남력
+//        private Integer spaceTimeScore;     // 시공간능력
+    }
 }
+
+
