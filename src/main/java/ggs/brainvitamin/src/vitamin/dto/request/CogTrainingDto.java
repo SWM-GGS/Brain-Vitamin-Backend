ed4662142e5330043b3eddabaf1ba5f3a026a4be
gyujegal <lkjdee0329@gmail.com>
2023-08-12 23:28:19 +0900

    Refactor: CogTrainingDto 유효성 검사 수정

--- src/main/java/ggs/brainvitamin/src/vitamin/dto/request/CogTrainingDto.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/dto/request/CogTrainingDto.java
@@ -2,6 +2,7 @@ package ggs.brainvitamin.src.vitamin.dto.request;
 
 import ggs.brainvitamin.config.Result;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
@@ -14,18 +15,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CogTrainingDto {
-    @NotBlank(message = "problem id 값을 입력해주세요")
+    @NotNull(message = "problem id 값을 입력해주세요")
     @Positive(message = "올바른 id 값을 입력해주세요")
     private Long problemId;
 
-    @NotBlank(message = "duration 값을 입력해주세요")
+    @NotNull(message = "duration 값을 입력해주세요")
     @Positive(message = "올바른 duration 값을 입력해주세요")
     private Float duration;
 
     @NotBlank(message = "result 값을 입력해주세요")
     private Result result;
 
-    @NotBlank(message = "score 값을 입력해주세요")
+    @NotNull(message = "score 값을 입력해주세요")
     @PositiveOrZero(message = "올바른 score 값을 입력해주세요")
     private Integer score;
 }
