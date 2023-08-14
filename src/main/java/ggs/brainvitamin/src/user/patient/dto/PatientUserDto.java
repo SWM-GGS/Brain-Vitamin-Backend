178e0f62319a3c3ec6fdab543eedb44b13cd9023
gyujegal <lkjdee0329@gmail.com>
2023-08-14 19:16:23 +0900

    Feat: PhoneNumberDto 유효성 검사 로직 추가

--- src/main/java/ggs/brainvitamin/src/user/patient/dto/PatientUserDto.java
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/PatientUserDto.java
@@ -1,5 +1,8 @@
 package ggs.brainvitamin.src.user.patient.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
@@ -30,6 +33,8 @@ public class PatientUserDto {
     @Data
     public static class PhoneNumberDto {
 
+        @NotBlank(message = "휴대폰 번호를 입력해주세요")
+        @Pattern(regexp = "01([0|1])([0-9]{8})")
         private String phoneNumber;
     }
 
