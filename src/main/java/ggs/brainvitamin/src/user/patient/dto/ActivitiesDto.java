14dd9482844207ca506cc3eeb3d3682068c67bce
gyujegal <lkjdee0329@gmail.com>
2023-08-15 00:31:20 +0900

    [#41]Refactor: GetScreeningTestHistoryDto testDate 속성 추가

--- src/main/java/ggs/brainvitamin/src/user/patient/dto/ActivitiesDto.java
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/ActivitiesDto.java
@@ -40,6 +40,7 @@ public class ActivitiesDto {
         private Integer totalScore;
         private String state;
         private String description = "";
+        private String testDate;
 
 //        private Integer attentionScore;     // 집중력
 //        private Integer executiveScore;     // 집행능력
