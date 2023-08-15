f2dcc7abc51b38cf7f7220a2834f9ab4e6e403be
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-08-15 17:48:46 +0900

    Refactor: 환자 우리가족 이야기 메인 조회 관련 가족 멤버 필드 추가

--- src/main/java/ggs/brainvitamin/src/post/patient/dto/PostMainDto.java
+++ src/main/java/ggs/brainvitamin/src/post/patient/dto/PostMainDto.java
@@ -1,6 +1,9 @@
 package ggs.brainvitamin.src.post.patient.dto;
 
+import ggs.brainvitamin.src.common.dto.CommonCodeDetailDto;
 import ggs.brainvitamin.src.common.entity.CommonCodeDetailEntity;
+import ggs.brainvitamin.src.user.patient.dto.FamilyMemberDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,6 +14,7 @@ import java.util.List;
 @Builder
 public class PostMainDto {
 
-    private List<EmotionInfoDto> emotionInfoDtoList = new ArrayList<>();
     private List<PostPreviewDto> postPreviewDtoList = new ArrayList<>();
+    private List<CommonCodeDetailDto> emotionInfoDtoList = new ArrayList<>();
+    private List<FamilyMemberDto> familyMemberDtoList = new ArrayList<>();
 }
--- src/main/java/ggs/brainvitamin/src/user/patient/dto/FamilyMemberDto.java
+++ src/main/java/ggs/brainvitamin/src/user/patient/dto/FamilyMemberDto.java
@@ -1,12 +1,16 @@
 package ggs.brainvitamin.src.user.patient.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class FamilyMemberDto {
 
+    private String name;
+    private String relationship;
+    private String profileImg;
 }
