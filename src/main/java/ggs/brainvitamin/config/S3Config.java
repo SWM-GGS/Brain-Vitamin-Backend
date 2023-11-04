daab3da6c5b8fa908398e4ffbcbae3eff0ee7069
gyujegal <lkjdee0329@gmail.com>
2023-11-04 23:11:37 +0900

    [#80]Feat: S3Config 추가 S3 접근을 위한 configuration 추가

---
+++ src/main/java/ggs/brainvitamin/config/S3Config.java
@@ -0,0 +1,38 @@
+package ggs.brainvitamin.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    /**
+     * application.yml 에 입력한 S3 정보를 이용해 AmazonS3Client 를 생성
+     *
+     * @return AmazonS3Client
+     */
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+}
