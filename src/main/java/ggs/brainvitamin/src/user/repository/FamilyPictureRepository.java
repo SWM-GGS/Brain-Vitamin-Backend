94d95be6019065e802fe6bfd89f9bbb404ea3c67
gyujegal <lkjdee0329@gmail.com>
2023-11-10 18:07:10 +0900

    [#116]Feat: FamilyPictureRepository 조회 로직 구현

--- src/main/java/ggs/brainvitamin/src/user/repository/FamilyPictureRepository.java
+++ src/main/java/ggs/brainvitamin/src/user/repository/FamilyPictureRepository.java
@@ -1,10 +1,15 @@
 package ggs.brainvitamin.src.user.repository;
 
+import ggs.brainvitamin.config.Status;
 import ggs.brainvitamin.src.user.entity.FamilyPictureEntity;
+import ggs.brainvitamin.src.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FamilyPictureRepository extends JpaRepository<FamilyPictureEntity, Long> {
 
+    List<FamilyPictureEntity> findAllByUserAndStatus(UserEntity userEntity, Status status);
 }
