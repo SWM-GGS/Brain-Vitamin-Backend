101d8c26aab8874bd7849b9d8978bdbdb1a1a1bc
gyujegal <lkjdee0329@gmail.com>
2023-11-23 16:22:18 +0900

    [#80]Refactor: ProblemRepository 메서드 추가 랜덤 문제 조회 로직 추가

--- src/main/java/ggs/brainvitamin/src/vitamin/repository/ProblemRepository.java
+++ src/main/java/ggs/brainvitamin/src/vitamin/repository/ProblemRepository.java
@@ -1,9 +1,17 @@
 package ggs.brainvitamin.src.vitamin.repository;
 
+import ggs.brainvitamin.src.vitamin.entity.PoolSfEntity;
 import ggs.brainvitamin.src.vitamin.entity.ProblemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<ProblemEntity, Long> {
+
+    @Query(value = "SELECT * FROM problem order by RAND() limit 8", nativeQuery = true)
+    List<ProblemEntity> findProblemsRandom8();
 }
